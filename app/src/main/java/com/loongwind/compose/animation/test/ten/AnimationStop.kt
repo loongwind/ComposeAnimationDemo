cb4ed11c61ef103ea7857860e3b29b3c07641c5e
loongwind <loongwind@gmail.com>
2023-03-25 17:39:29 +0800

    add stop animation

--- app/src/main/java/com/loongwind/compose/animation/test/ten/AnimationStop.kt
+++ app/src/main/java/com/loongwind/compose/animation/test/ten/AnimationStop.kt
@@ -1,5 +1,6 @@
 package com.loongwind.compose.animation.test
 
+import android.util.Log
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.VectorConverter
 import androidx.compose.animation.core.tween
@@ -20,6 +21,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.launch
 
 
@@ -75,10 +77,14 @@ fun AnimationInterrupt() {
                     moveToRight = !moveToRight
                     backgroundColor = Color.Blue
                     scope.launch {
-                        animatable.animateTo(
-                            if (moveToRight) 200.dp else 10.dp,
-                            animationSpec = tween(durationMillis = 1000)
-                        )
+                        try {
+                            animatable.animateTo(
+                                if (moveToRight) 200.dp else 10.dp,
+                                animationSpec = tween(durationMillis = 1000)
+                            )
+                        } catch (e: CancellationException) {
+                            Log.e("触发", "CancellationException")
+                        }
                     }
                 }
         )
@@ -92,6 +98,68 @@ fun AnimationInterrupt() {
         }
     }
 
+}
+
+@Preview
+@Composable
+fun StopAnimation() {
+    // 创建状态 通过状态驱动动画
+    var moveToRight by remember { mutableStateOf(false) }
+    var backgroundColor by remember { mutableStateOf(Color.Blue) }
+    // 动画实例
+    val animatable = remember { Animatable(10.dp, Dp.VectorConverter) }
+
+    val scope = rememberCoroutineScope()
+
+    Box {
+        Box(
+            Modifier
+                .padding(start = 10.dp)
+                .size(1.dp, 160.dp)
+        ){
+            VerticalDottedLine()
+        }
+        Box(
+            Modifier
+                .padding(start = 200.dp)
+                .size(1.dp, 160.dp)
+        ){
+            VerticalDottedLine()
+        }
+
+        Text(text = "10dp", Modifier.padding(start = 0.dp, top = 160.dp), style = TextStyle(fontSize = 8.sp))
+        Text(text = "200dp", Modifier.padding(start = 190.dp, top = 160.dp), style = TextStyle(fontSize = 8.sp))
+        Box(
+            Modifier
+                // 使用动画值
+                .padding(start = animatable.value, top = 30.dp)
+                .size(100.dp, 100.dp)
+                .background(backgroundColor)
+                .clickable {
+                    // 修改状态
+                    moveToRight = !moveToRight
+                    backgroundColor = Color.Blue
+                    scope.launch {
+                        try {
+                            animatable.animateTo(
+                                if (moveToRight) 200.dp else 10.dp,
+                                animationSpec = tween(durationMillis = 1000)
+                            )
+                        } catch (e: CancellationException) {
+                            Log.e("触发", "CancellationException")
+                        }
+                    }
+                }
+        )
+        Button(onClick = {
+            backgroundColor = Color.Cyan
+            scope.launch {
+                animatable.stop()
+            }
+        }, Modifier.padding(top = 170.dp, start = 70.dp)) {
+            Text(text = "Stop", style = TextStyle(fontSize = 10.sp))
+        }
+    }
 
 }
 
