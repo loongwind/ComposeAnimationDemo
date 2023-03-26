40c96925d5d7f1fcba25ce711fe6a9d1ded440e8
loongwind <loongwind@gmail.com>
2023-03-26 14:44:43 +0800

    add bound animation demo

--- app/src/main/java/com/loongwind/compose/animation/test/ten/AnimationStop.kt
+++ app/src/main/java/com/loongwind/compose/animation/test/ten/AnimationStop.kt
@@ -1,9 +1,9 @@
 package com.loongwind.compose.animation.test
 
 import android.util.Log
-import androidx.compose.animation.core.Animatable
-import androidx.compose.animation.core.VectorConverter
-import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.*
+import androidx.compose.animation.rememberSplineBasedDecay
+import androidx.compose.animation.splineBasedDecay
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -14,11 +14,15 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.CancellationException
@@ -163,6 +167,230 @@ fun StopAnimation() {
 
 }
 
+
+@Preview
+@Composable
+fun AnimationBound() {
+    // 创建状态 通过状态驱动动画
+    var moveToRight by remember { mutableStateOf(false) }
+    var backgroundColor by remember { mutableStateOf(Color.Blue) }
+    // 动画实例
+    val animatable = remember { Animatable(10.dp, Dp.VectorConverter) }
+    animatable.updateBounds(upperBound = 200.dp, lowerBound = 10.dp)
+    val scope = rememberCoroutineScope()
+
+    Box {
+        Box(
+            Modifier
+                .padding(start = 10.dp)
+                .size(1.dp, 160.dp)
+        ){
+            VerticalDottedLine()
+        }
+        Box(
+            Modifier
+                .padding(start = 200.dp)
+                .size(1.dp, 160.dp)
+        ){
+            VerticalDottedLine()
+        }
+        Box(
+            Modifier
+                .padding(start = 400.dp)
+                .size(1.dp, 160.dp)
+        ){
+            VerticalDottedLine()
+        }
+
+        Text(text = "10dp", Modifier.padding(start = 0.dp, top = 160.dp), style = TextStyle(fontSize = 8.sp))
+        Text(text = "200dp", Modifier.padding(start = 190.dp, top = 160.dp), style = TextStyle(fontSize = 8.sp))
+        Text(text = "400dp", Modifier.padding(start = 390.dp, top = 160.dp), style = TextStyle(fontSize = 8.sp))
+        Box(
+            Modifier
+                // 使用动画值
+                .padding(start = animatable.value, top = 30.dp)
+                .size(100.dp, 100.dp)
+                .background(backgroundColor)
+                .clickable {
+                    // 修改状态
+                    moveToRight = !moveToRight
+                    backgroundColor = Color.Blue
+                    scope.launch {
+                        try {
+                            val animationResult = animatable.animateTo(
+                                if (moveToRight) 400.dp else -100.dp,
+                                animationSpec = tween(durationMillis = 1000)
+                            )
+                            if(animationResult.endReason == AnimationEndReason.BoundReached){
+                                // do something
+                            }
+                        } catch (e: CancellationException) {
+                            Log.e("触发", "CancellationException")
+                        }
+                    }
+                }
+        )
+    }
+
+}
+
+
+@Preview
+@Composable
+fun AnimationBound2() {
+    // 创建状态 通过状态驱动动画
+    var moveToRight by remember { mutableStateOf(false) }
+    // 动画实例
+    val animatable = remember { Animatable(Offset(10f, 30f), Offset.VectorConverter) }
+    animatable.updateBounds(lowerBound = Offset(10f, 30f), upperBound = Offset(400f, 200f))
+    val scope = rememberCoroutineScope()
+
+    Box {
+        Box(
+            Modifier
+                .padding(start = 10.dp)
+                .size(1.dp, 320.dp)
+        ){
+            VerticalDottedLine()
+        }
+        Box(
+            Modifier
+                .padding(start = 200.dp)
+                .size(1.dp, 320.dp)
+        ){
+            VerticalDottedLine()
+        }
+        Box(
+            Modifier
+                .padding(start = 400.dp)
+                .size(1.dp, 320.dp)
+        ){
+            VerticalDottedLine()
+        }
+        Box(
+            Modifier
+                .padding(top = 30.dp)
+                .size(400.dp, 1.dp)
+        ){
+            HorizontalDottedLine()
+        }
+        Box(
+            Modifier
+                .padding(top = 200.dp)
+                .size(400.dp, 1.dp)
+        ){
+            HorizontalDottedLine()
+        }
+
+        Text(text = "10dp", Modifier.padding(start = 0.dp, top = 320.dp), style = TextStyle(fontSize = 8.sp))
+        Text(text = "200dp", Modifier.padding(start = 190.dp, top = 320.dp), style = TextStyle(fontSize = 8.sp))
+        Text(text = "400dp", Modifier.padding(start = 390.dp, top = 320.dp), style = TextStyle(fontSize = 8.sp))
+        Text(text = "30dp", Modifier.padding(start = 330.dp, top = 15.dp), style = TextStyle(fontSize = 8.sp))
+        Text(text = "200dp", Modifier.padding(start = 325.dp, top = 185.dp), style = TextStyle(fontSize = 8.sp))
+        Box(
+            Modifier
+                // 使用动画值
+                .padding(start = animatable.value.x.dp, top = animatable.value.y.dp)
+                .size(100.dp, 100.dp)
+                .background(Color.Blue)
+                .clickable {
+                    // 修改状态
+                    moveToRight = !moveToRight
+                    scope.launch {
+                        try {
+                            animatable.animateTo(
+                                if (moveToRight) Offset(400f,400f) else Offset(-100f,0f),
+                                animationSpec = tween(durationMillis = 1000)
+                            )
+                        } catch (e: CancellationException) {
+                            Log.e("触发", "CancellationException")
+                        }
+                    }
+                }
+        )
+    }
+
+}
+
+
+@Preview
+@Composable
+fun AnimationBound3() {
+    // 创建状态 通过状态驱动动画
+    var moveToRight by remember { mutableStateOf(false) }
+    // 动画实例
+    val animatable = remember { Animatable(10.dp, Dp.VectorConverter) }
+    animatable.updateBounds(upperBound = 200.dp, lowerBound = 10.dp)
+    val scope = rememberCoroutineScope()
+    val splineBasedDecay = rememberSplineBasedDecay<Dp>()
+
+    Box {
+        Box(
+            Modifier
+                .padding(start = 10.dp)
+                .size(1.dp, 160.dp)
+        ){
+            VerticalDottedLine()
+        }
+        Box(
+            Modifier
+                .padding(start = 200.dp)
+                .size(1.dp, 160.dp)
+        ){
+            VerticalDottedLine()
+        }
+        Box(
+            Modifier
+                .padding(start = 400.dp)
+                .size(1.dp, 160.dp)
+        ){
+            VerticalDottedLine()
+        }
+
+        Text(text = "10dp", Modifier.padding(start = 0.dp, top = 160.dp), style = TextStyle(fontSize = 8.sp))
+        Text(text = "200dp", Modifier.padding(start = 190.dp, top = 160.dp), style = TextStyle(fontSize = 8.sp))
+        Text(text = "400dp", Modifier.padding(start = 390.dp, top = 160.dp), style = TextStyle(fontSize = 8.sp))
+        Box(
+            Modifier
+                // 使用动画值
+                .padding(start = animatable.value, top = 30.dp)
+                .size(100.dp, 100.dp)
+                .background(Color.Blue)
+                .clickable {
+                    // 修改状态
+                    moveToRight = !moveToRight
+                    scope.launch {
+                        try {
+                            if(moveToRight){
+                                val animationResult = animatable.animateDecay(3000.dp, splineBasedDecay)
+                                if(animationResult.endReason == AnimationEndReason.BoundReached){
+                                    reverseAnimation(animatable, -animationResult.endState.velocity, splineBasedDecay)
+                                }
+                            }else{
+                                animatable.snapTo(10.dp)
+                            }
+
+                        } catch (e: CancellationException) {
+                            Log.e("触发", "CancellationException")
+                        }
+                    }
+                }
+        )
+    }
+
+}
+
+private suspend fun reverseAnimation(
+    animatable: Animatable<Dp, AnimationVector1D>,
+    initialVelocity: Dp,
+    splineBasedDecay: DecayAnimationSpec<Dp>
+) {
+    val result =  animatable.animateDecay(initialVelocity, splineBasedDecay)
+    if(result.endReason == AnimationEndReason.BoundReached){
+        reverseAnimation(animatable, -result.endState.velocity, splineBasedDecay)
+    }
+}
+
 @Composable
 private fun VerticalDottedLine() {
     val pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f)
@@ -174,6 +402,21 @@ private fun VerticalDottedLine() {
             color = Color.Red,
             start = Offset(0f, 0f),
             end = Offset(0f, size.height),
+            pathEffect = pathEffect
+        )
+    }
+}
+@Composable
+private fun HorizontalDottedLine() {
+    val pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f)
+    Canvas(
+        Modifier
+            .fillMaxWidth()
+            .height(1.dp)) {
+        drawLine(
+            color = Color.Red,
+            start = Offset(0f, 0f),
+            end = Offset(size.width, 0f),
             pathEffect = pathEffect
         )
     }
